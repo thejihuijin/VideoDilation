1920a1da234a243b923540b956314aa20eb0fbda
ErikJ <ejorgensen7@gatech.edu>
2017-11-12 22:29:07 -0500

    Added Video playing function that handles RGB or Gray videos

---
+++ videoDilation/playVidMat.m
@@ -0,0 +1,36 @@
+function [] = playVidMat(vidMat, frameRates)
+if length(size(vidMat)) == 4
+    [~, ~, ~, frames] = size(vidMat);
+else
+    [~, ~, frames] = size(vidMat);
+end
+
+if length(frameRates) == 1
+    frameRates = frameRates * ones(1,frames);
+end
+if frames ~= length(frameRates)
+    error('Framerate vector and video matrix not equal length.')
+end
+
+figure
+currTime = 0;
+if length(size(vidMat)) == 4
+    for i = 1:frames
+        imagesc(vidMat(:,:,:,i))
+        axis off
+        title([sprintf('%.2f',currTime) ' seconds elapsed'])
+        pause( 1/frameRates(i) );
+        currTime = currTime + 1/frameRates(i);
+    end
+else
+    for i = 1:frames
+        colormap gray
+        imagesc(vidMat(:,:,i))
+        title([sprintf('%.2f',currTime) ' seconds elapsed'])
+        pause( 1/frameRates(i) );
+        currTime = currTime + 1/frameRates(i);
+    end
+end      
+
+end
+
