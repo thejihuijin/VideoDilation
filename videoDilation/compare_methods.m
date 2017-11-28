effe45f28e34fbaf866850bf03bab1e69e92a304
Jihui Jin <thejihuijin@gmail.com>
2017-11-27 23:38:56 -0500

    Add Video Playback and quantization
    add Erik's code for quantization and constant frame playback.
    save all functions

--- videoDilation/compare_methods.m
+++ videoDilation/compare_methods.m
@@ -17,20 +17,24 @@ clear all; close all; clc;
 % [rows, cols, ~, n_frames] = size(rgbVid);
 % vid = rgbToGrayVid(rgbVid);
 %% Load Video
-filename = '../data/Reddit_Videos/dog_and_stuffedDog.mp4';
+basefile = 'rs_cat_wall_climb';
+filename = strcat(basefile,'.mp4');
 dim_ds = 2;
 
 [rgbvid, fr] = sliceVid(filename,0,20,dim_ds);
 vid = rgbToGrayVid(rgbvid);
 [rows, cols, n_frames] = size(vid);
 clear('rgbvid');
-%% Compute Saliency
 
-iFName = 'dog_and_stuffedDog.mp4';%'ball_slowmo_crop.mp4';
-oFName = 'dasd_saliency.mpg';
-oMFName = 'dasd_saliency.mat';
+%%
+resize_vid(filename,'rs_cat_wall_climb.mp4',960,720);
+%% Compute Saliency
+clc;
+iFName = filename;
+oFName = strcat(basefile,'_saliency.avi');
+oMFName = strcat(basefile,'_saliency.mat');
 wsize_s=3; wsize_t=3; wsize_fs=5; wsize_ft=5; 
-scaleFactor=1/8; segLength=min(n_frames,300);
+scaleFactor=1/8; segLength=min(n_frames,150);
 detSal(iFName,oFName,oMFName,wsize_s,wsize_t,...
         wsize_fs,wsize_ft,scaleFactor,segLength);
 fprintf('Done\n');
@@ -47,7 +51,7 @@ clear('regex','wsize_*','segLength','scaleFactor','*FName');
 % end
 % saliencyMapHolder = saliencyMapHolder(:,:,1:2:end-1);
 
-load('dasd_saliency_1.mat');
+load(strcat(basefile,'_saliency_1.mat'));
 saliencyMapHolder = salMap(:,:,:);
 
 saliencyMapTime = 1/12*(salMapSI + salMapSQ + salMapSY) ...
@@ -57,18 +61,19 @@ saliencyMapS = 1/3*(salMapSI + salMapSQ + salMapSY);
 clear('regex','salMap*');
 size(saliencyMapHolder)
 %% Determine "ground truth"
-% f=figure; colormap('gray');
-% for i=1:n_frames
-%     imagesc(vid(:,:,i))
-%     %title('Constant framerate')
-%     title(['Frame ',num2str(i)])
-%     keydown = waitforbuttonpress;
-% end
-% close(f);
-% dasd_gt = ones(1,n_frames)*.5;
-% dasd_gt(210:270) = 1;
-% save('dog_and_stuffedDog_gt.mat','dasd_gt');
-load('dog_and_stuffedDog_gt.mat');
+f=figure; colormap('gray');
+for i=1:n_frames
+    imagesc(vid(:,:,i))
+    %title('Constant framerate')
+    title(['Frame ',num2str(i)])
+    keydown = waitforbuttonpress;
+end
+close(f);
+%%
+gt = ones(1,n_frames)*.5;
+gt(80:end) = 1;
+save(strcat(basefile,'_gt.mat'),'gt');
+% load('dog_and_stuffedDog_gt.mat');
 %% Compute optical flow frames
 % Horn-Schunck Method
 OF = opticalFlowHS();
@@ -150,6 +155,51 @@ tsal_five_num = five_num_sum(saliencyMapTime);
 tsal_weight_pool = weight_pool(saliencyMapTime,2);
 tsal_weight_pool_half = weight_pool(saliencyMapTime,.5);
 
+
+
+
+%% Compare
+figure; hold on
+if exist('gt','var')
+    plot(gt,'DisplayName','Ground Truth');
+end
+plot(of_total_energy,'DisplayName','OF Total');
+plot(of_minkowski,'DisplayName','OF Minkowski');
+plot(of_min_half,'DisplayName','OF Minkowski 1/2');
+plot(of_five_num,'DisplayName','OF 5 Num Sum');
+plot(of_weight_pool,'DisplayName','OF Weight Pool 2');
+plot(of_weight_pool_half,'DisplayName','OF Weight Pool 1/2');
+legend();
+
+title('OF Energy Functions');
+figure; hold on
+if exist('gt','var')
+    plot(gt,'DisplayName','Ground Truth');
+end
+plot(sal_energy,'DisplayName','sal EMD');
+plot(sal_total_energy,'DisplayName','sal Total');
+plot(sal_minkowski,'DisplayName','sal Minkowski');
+plot(sal_min_half,'DisplayName','sal Minkowski 1/2');
+plot(sal_five_num,'DisplayName','sal 5 Num Sum');
+plot(sal_weight_pool,'DisplayName','sal Weight Pool 2');
+plot(sal_weight_pool_half,'DisplayName','sal Weight Pool 1/2');
+legend();
+title('Sal Energy Functions');
+
+figure; hold on
+if exist('gt','var')
+    plot(gt,'DisplayName','Ground Truth');
+end
+plot(tsal_energy,'DisplayName','tsal EMD');
+plot(tsal_total_energy,'DisplayName','tsal Total');
+plot(tsal_minkowski,'DisplayName','tsal Minkowski');
+plot(tsal_min_half,'DisplayName','tsal Minkowski 1/2');
+plot(tsal_five_num,'DisplayName','tsal 5 Num Sum');
+plot(tsal_weight_pool,'DisplayName','tsal Weight Pool 2');
+plot(tsal_weight_pool_half,'DisplayName','tsal Weight Pool 1/2');
+legend();
+title('Time Saliency Energy Functions');
+
 %% Compute Saliency Masking
 masked_flow = zeros(size(flow_mags));
 masked_tsal = zeros(size(saliencyMapTime));
@@ -162,7 +212,22 @@ for i=1:n_frames
     sal_mask = imbinarize(saliencyMapHolder(:,:,i));
     masked_tsal(:,:,i) = saliencyMapTime(:,:,i).*sal_mask;
 end
-
+%% Compute Masked Time-Weighted Saliency Energy
+mtsal_energy = saliency_EMD_Avg_Energy(masked_tsal);
+mtsal_total_energy = total_Energy(masked_tsal);
+mtsal_minkowski = minkowski(masked_tsal);
+mtsal_min_half = minkowski(masked_tsal,.5);
+mtsal_five_num = five_num_sum(masked_tsal);
+mtsal_weight_pool = weight_pool(masked_tsal,2);
+mtsal_weight_pool_half = weight_pool(masked_tsal,.5);
+%% Compute Masked OF Energy
+mof_energy = saliency_EMD_Avg_Energy(masked_flow);
+mof_total_energy = total_Energy(masked_flow);
+mof_minkowski = minkowski(masked_flow);
+mof_min_half = minkowski(masked_flow,.5);
+mof_five_num = five_num_sum(masked_flow);
+mof_weight_pool = weight_pool(masked_flow,2);
+mof_weight_pool_half = weight_pool(masked_flow,.5);
 %%
 sal_masks = zeros(size(vid));
 sal_masksS = zeros(size(vid));
@@ -185,62 +250,11 @@ for i=1:n_frames
     title('Spatial Saliency');
     pause(1/10);
 end
-%% Compute Masked Time-Weighted Saliency Energy
-mtsal_energy = saliency_EMD_Avg_Energy(masked_tsal);
-mtsal_total_energy = total_Energy(masked_tsal);
-mtsal_minkowski = minkowski(masked_tsal);
-mtsal_min_half = minkowski(masked_tsal,.5);
-mtsal_five_num = five_num_sum(masked_tsal);
-mtsal_weight_pool = weight_pool(masked_tsal,2);
-mtsal_weight_pool_half = weight_pool(masked_tsal,.5);
-
-%% Compute Masked OF Energy
-mof_energy = saliency_EMD_Avg_Energy(masked_flow);
-mof_total_energy = total_Energy(masked_flow);
-mof_minkowski = minkowski(masked_flow);
-mof_min_half = minkowski(masked_flow,.5);
-mof_five_num = five_num_sum(masked_flow);
-mof_weight_pool = weight_pool(masked_flow,2);
-mof_weight_pool_half = weight_pool(masked_flow,.5);
-%% Compare
-figure; hold on
-plot(dasd_gt,'DisplayName','Ground Truth');
-plot(of_total_energy,'DisplayName','OF Total');
-plot(of_minkowski,'DisplayName','OF Minkowski');
-plot(of_min_half,'DisplayName','OF Minkowski 1/2');
-plot(of_five_num,'DisplayName','OF 5 Num Sum');
-plot(of_weight_pool,'DisplayName','OF Weight Pool 2');
-plot(of_weight_pool_half,'DisplayName','OF Weight Pool 1/2');
-legend();
-
-title('OF Energy Functions');
-figure; hold on
-plot(dasd_gt,'DisplayName','Ground Truth');
-plot(sal_energy,'DisplayName','sal EMD');
-plot(sal_total_energy,'DisplayName','sal Total');
-plot(sal_minkowski,'DisplayName','sal Minkowski');
-plot(sal_min_half,'DisplayName','sal Minkowski 1/2');
-plot(sal_five_num,'DisplayName','sal 5 Num Sum');
-plot(sal_weight_pool,'DisplayName','sal Weight Pool 2');
-plot(sal_weight_pool_half,'DisplayName','sal Weight Pool 1/2');
-legend();
-title('Sal Energy Functions');
-
-figure; hold on
-plot(dasd_gt,'DisplayName','Ground Truth');
-plot(tsal_energy,'DisplayName','tsal EMD');
-plot(tsal_total_energy,'DisplayName','tsal Total');
-plot(tsal_minkowski,'DisplayName','tsal Minkowski');
-plot(tsal_min_half,'DisplayName','tsal Minkowski 1/2');
-plot(tsal_five_num,'DisplayName','tsal 5 Num Sum');
-plot(tsal_weight_pool,'DisplayName','tsal Weight Pool 2');
-plot(tsal_weight_pool_half,'DisplayName','tsal Weight Pool 1/2');
-legend();
-title('Time Saliency Energy Functions');
 %%
 figure; hold on
-plot(dasd_gt,'DisplayName','Ground Truth');
-plot(mtsal_energy,'DisplayName','mtsal EMD');
+if exist('gt','var')
+    plot(gt,'DisplayName','Ground Truth');
+end
 plot(mtsal_total_energy,'DisplayName','mtsal Total');
 plot(mtsal_minkowski,'DisplayName','mtsal Minkowski');
 plot(mtsal_min_half,'DisplayName','mtsal Minkowski 1/2');
@@ -251,7 +265,9 @@ legend();
 title('Masked Time Saliency Energy Functions');
 
 figure; hold on
-plot(dasd_gt,'DisplayName','Ground Truth');
+if exist('gt','var')
+    plot(gt,'DisplayName','Ground Truth');
+end
 plot(mof_energy,'DisplayName','mof EMD');
 plot(mof_total_energy,'DisplayName','mof Total');
 plot(mof_minkowski,'DisplayName','mof Minkowski');
@@ -263,28 +279,67 @@ legend();
 title('Masked Optical Flow Energy Functions');
 %%
 clear('regex','of_*','sal_*','tsal_*','mof_*','mtsal_*');
-%% Compare two graphs
-of_fr = 4*(mean(of_energy)-of_energy);
-of_fr = 30*2.^(of_fr);
-sal_fr = 4*(mean(sal_energy)-sal_energy);
-sal_fr = 30*2.^(sal_fr);
+%%
+%
+%
+%                 PLAYBACK VIDEOS
+%
+%
+%%
+clc; close all;
+%%
+energy_normal = 1-weight_pool(masked_flow,.5);
 
-figure; 
-subplot(121); hold on
-plot(of_energy);
-plot(sal_energy);
-legend('Optical Flow','Saliency');
-title('Energy Functions');
+%% Scale framerate to desired range
+% Framerate normalized to range between min_fr and min_fr + range_fr
+min_fr = 15;
+range_fr = 60;
+fr_scaled = energy_normal*range_fr + min_fr;
+fr_scaled2 = 30.*2.^((-1+2*energy_normal));
 
-subplot(122); hold on
-plot(of_fr);
-plot(sal_fr);
-legend('Optical Flow','Saliency');
-title('Frame Rates');
+figure; hold on
+plot(fr_scaled,'DisplayName','Linear');
+plot(fr_scaled2,'DisplayName','Exp');
+legend()
 
+% Alternatively, scale so that the mean framerate = original framerate
+% energy_normal2 = energy_normal / mean(energy_normal);
+% fr_scaled2 = min_fr + (fr - min_fr)*energy_normal2;
 
+%% Smooth, renormalize, and scale the energy-normalized framerate
+mov_avg_window = 15;
 
+%energy_smooth = movmean(energy_normal, mov_avg_window);
+%energy_smooth = (energy_smooth - min(energy_smooth))/range(energy_smooth);
+%fr_smoothed = energy_smooth*range_fr + min_fr;
+fr_smoothed = movmean(fr_scaled, mov_avg_window);
+fr_smoothed2 = movmean(fr_scaled2, mov_avg_window);
+% Alternatively, scale so that the mean framerate = original framerate
+% energy_smoothed2 = movmean( energy_normal, mov_avg_window);
+% energy_smoothed2 = energy_smoothed2 / mean(energy_smoothed2);
+% fr_smoothed2 = min_fr + (fr - min_fr)*energy_smoothed2;
 
+%% Quantize framerate, adjust framerate changes, re-smooth framerate
+q_levels = 5;
+fr_quant = quantFR( fr_smoothed, q_levels );
+fr_q_adj = adjustFR( fr_quant, 0.1, fr );
+
+% Smooth the adjusted framerate
+mov_avg_window = 5;
+fr_q_adj_smooth = movmean( fr_q_adj, mov_avg_window );
+fr_q_adj_smooth = movmean( fr_q_adj_smooth, mov_avg_window );
+
+figure
+plot(1:n_frames,fr_smoothed, 1:n_frames,fr_q_adj, 1:n_frames, fr_q_adj_smooth)
+legend('Smoothed', 'Adjusted, Quantized', 'Adjusted, Quantized, Smoothed')
+
+%% Compute playback vector from framerate vector
+scaled_playback = fr2playback(fr_scaled, fr);
+smoothed_playback = fr2playback(fr_smoothed, fr);
+adjusted_smooth_playback = fr2playback(fr_q_adj_smooth, fr);
+%% Play Video
+figure
+playDilatedFrames( vid, adjusted_smooth_playback, fr, fr_q_adj_smooth )
 %% Play Videos
 figure, colormap('Gray');
 currTime = 0;
---
+++ videoDilation/resize_vid.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = resize_vid(inputName,outputName,newrows,newcols)
+inputReader = VideoReader(inputName);
+
+outputWriter = VideoWriter(outputName);
+open(outputWriter);
+
+while hasFrame(inputReader)
+	frame = readFrame(inputReader);
+	outputFrame = imresize(frame, [newrows, newcols]);
+	writeVideo(outputWriter, outputFrame);
+	
+end
+close(outputWriter);
+fprintf('done\n');
+end
+
